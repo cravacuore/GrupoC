d79528a1b2648f9fbe179d24a0cca191a07aa597
Jonakoelbl <jonakoelbl@gmail.com>
2014-05-15 17:27:08 -0300

    Change presentation

--- GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/tabs/RankingBorrowedBookPanel.scala
+++ GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/tabs/RankingBorrowedBookPanel.scala
@@ -10,25 +10,25 @@ import ar.edu.unq.desapp.model.bean.Book
 import ar.edu.unq.desapp.services.bean.BookService
 import ar.edu.unq.desapp.utils.DSLWicket
 
-@SerialVersionUID(123412341234L)
-class RankingBorrowedBookPanel(var idPanel: String, var service: BookService) extends Panel(idPanel) with DSLWicket{
+//@SerialVersionUID(123412341234L)
+class RankingBorrowedBookPanel(var idPanel: String) extends Panel(idPanel) {
   
-  val form = new Form[BookService]("BookServiceForm", new CompoundPropertyModel[BookService](service))
+//  val form = new Form[BookService]("BookServiceForm", new CompoundPropertyModel[BookService](service))
+//  
+//  this.createTableRanking(form)
   
-  this.createTableRanking(form)
-  
-  protected def createTableRanking(form: Form[BookService]) {
-    form.add(new PropertyListView[Book]("retriveAllMostBorrowed") {
-      val serialVersionUID = 1L
-
-      override def populateItem(item: ListItem[Book]) {
-        item.add(label("title"))
-        item.add(label("isbn"))
-//        item.add(label("editorial"))
-//        item.add(label("description"))
-//        item.add(image)
-      }
-    })
-  }
+//  protected def createTableRanking(form: Form[BookService]) {
+//    form.add(new PropertyListView[Book]("retriveAllMostBorrowed") {
+//      val serialVersionUID = 1L
+//
+//      override def populateItem(item: ListItem[Book]) {
+//        item.add(label("title"))
+//        item.add(label("isbn"))
+////        item.add(label("editorial"))
+////        item.add(label("description"))
+////        item.add(image)
+//      }
+//    })
+//  }
   
 }
