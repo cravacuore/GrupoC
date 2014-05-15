4da1669b14e65ee4240db16f8e61b7ad00c5808b
Jonakoelbl <jonakoelbl@gmail.com>
2014-05-15 17:33:26 -0300

    Change wicket model

--- GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/model/Home.scala
+++ GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/model/Home.scala
@@ -1,47 +1,49 @@
 package ar.edu.unq.desapp.view.model
 
 import scala.collection.JavaConverters.seqAsJavaListConverter
-
 import org.apache.wicket.extensions.markup.html.tabs.AbstractTab
 import org.apache.wicket.extensions.markup.html.tabs.TabbedPanel
 import org.apache.wicket.markup.html.WebPage
 import org.apache.wicket.markup.html.basic.Label
-import org.apache.wicket.markup.html.form.Form
 import org.apache.wicket.markup.html.panel.Panel
-import org.apache.wicket.model.CompoundPropertyModel
 import org.apache.wicket.model.Model
 import org.apache.wicket.spring.injection.annot.SpringBean
-
-import ar.edu.unq.desapp.services.GeneralService
+//import ar.edu.unq.desapp.services.GeneralService
 import ar.edu.unq.desapp.utils.DSLWicket
 import ar.edu.unq.desapp.view.tabs.ManageBookPanel
+import ar.edu.unq.desapp.view.tabs.RankingBorrowedBookPanel
+import ar.edu.unq.desapp.view.tabs.ManageBookPanel
+import ar.edu.unq.desapp.view.tabs.RankingBorrowedBookPanel
+import ar.edu.unq.desapp.view.tabs.RankingBorrowedBookPanel
+import ar.edu.unq.desapp.view.tabs.ManageBookPanel
+
 
 @SerialVersionUID(9798132401L)
 class Home extends WebPage with DSLWicket {
 
-  @SpringBean(name = "services.general")
-  var generalService: GeneralService = new GeneralService
+//  @SpringBean(name = "services.general")
+//  var generalService: GeneralService = new GeneralService
 
   var message: Label = null
 
   override def onInitialize {
     super.onInitialize
-    val form = new Form[GeneralService]("GeneralServiceForm", new CompoundPropertyModel[GeneralService](this.generalService))
-    this.createTabsBook(form)
-    add(form)
+    add(this.createTabs)
   }
-
-  protected def createTabsBook(form: Form[GeneralService]) {
+  
+  protected def createTabs: TabbedPanel = {
     var tabs: List[AbstractTab] = List(
-        (new AbstractTab(new Model[String]("Manage Book")) {
-        def getPanel(panelId: String): Panel = { return new ManageBookPanel(panelId, generalService.bookService) }
+        (new AbstractTab(new Model("Manage Book")) {
+        	def getPanel(panelId: String): Panel = { 
+        	  return new ManageBookPanel(panelId) 
+        	}
         }),
-        (new AbstractTab(new Model[String]("Ranking of the most borrowed")) {
-        def getPanel(panelId: String): Panel = { return new ManageBookPanel(panelId, generalService.bookService) }
+        (new AbstractTab(new Model("Ranking")) {
+        	def getPanel(panelId: String): Panel = { 
+        	  new RankingBorrowedBookPanel(panelId) 
+        	}
         })
     )
-    
-    add(new TabbedPanel("tabs", tabs.asJava))
-  }
- 
+    new TabbedPanel("tabs", tabs.asJava)
+  } 
 }
--- GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/tabs/RankingBorrowedBookPanel.scala
+++ GrupoC-Presentation/src/main/java/ar/edu/unq/desapp/view/tabs/RankingBorrowedBookPanel.scala
@@ -4,28 +4,31 @@ import org.apache.wicket.markup.html.form.Form
 import org.apache.wicket.markup.html.list.ListItem
 import org.apache.wicket.markup.html.list.PropertyListView
 import org.apache.wicket.markup.html.panel.Panel
+import org.apache.wicket.model.CompoundPropertyModel
 
 import ar.edu.unq.desapp.model.bean.Book
 import ar.edu.unq.desapp.services.bean.BookService
 import ar.edu.unq.desapp.utils.DSLWicket
 
-@SerialVersionUID(123412341234L)
-class RankingBorrowedBookPanel(var idPanel: String, var form: Form[BookService]) extends Panel(idPanel) with DSLWicket{
+//@SerialVersionUID(123412341234L)
+class RankingBorrowedBookPanel(var idPanel: String) extends Panel(idPanel) {
   
-  this.createTableRanking(form)
+//  val form = new Form[BookService]("BookServiceForm", new CompoundPropertyModel[BookService](service))
+//  
+//  this.createTableRanking(form)
   
-  protected def createTableRanking(form: Form[BookService]) {
-    form.add(new PropertyListView[Book]("bookService.retriveAllMostBorrowed") {
-      val serialVersionUID = 1L
-
-      override def populateItem(item: ListItem[Book]) {
-        item.add(label("title"))
-        item.add(label("isbn"))
-        item.add(label("editorial"))
-        item.add(label("description"))
-//        item.add(image)
-      }
-    })
-  }
+//  protected def createTableRanking(form: Form[BookService]) {
+//    form.add(new PropertyListView[Book]("retriveAllMostBorrowed") {
+//      val serialVersionUID = 1L
+//
+//      override def populateItem(item: ListItem[Book]) {
+//        item.add(label("title"))
+//        item.add(label("isbn"))
+////        item.add(label("editorial"))
+////        item.add(label("description"))
+////        item.add(image)
+//      }
+//    })
+//  }
   
 }
